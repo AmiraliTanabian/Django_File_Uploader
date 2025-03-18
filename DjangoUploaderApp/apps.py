d49f433743d8bcb9f533b198d026a9545e972df8
Amirali <atanabain@gmail.com>
2025-03-19 02:01:13 +0330

    Rename app

--- DjangoUploaderApp/apps.py
+++ DjangoUploaderApp/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DjangouloaderappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'DjangoUloaderApp'
+    name = 'DjangoUploaderApp'
