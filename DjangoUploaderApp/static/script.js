abbc2d60bb9319f348d01309d92e2bc72ca64172
Amirali <atanabain@gmail.com>
2025-03-19 02:45:29 +0330

    Create script for error part and set 'disable' for submit button when file does not exists

--- DjangoUploaderApp/static/script.js
+++ DjangoUploaderApp/static/script.js
@@ -1,4 +1,20 @@
 document.getElementById('file-upload').addEventListener('change', function(event) {
-    const fileName = event.target.files[0] ? event.target.files[0].name : 'هیچ فایلی انتخاب نشده است';
+    const fileInput = event.target;
+    const fileName = fileInput.files.length > 0 ? fileInput.files[0].name : 'هیچ فایلی انتخاب نشده است';
     document.getElementById('file-name').textContent = fileName;
+
+    const submitBtn = document.getElementById('submit-btn');
+    if (fileInput.files.length > 0) {
+        submitBtn.removeAttribute('disabled');
+    } else {
+        submitBtn.setAttribute('disabled', 'true');
+    }
 });
+
+setTimeout(function() {
+    var errorBox = document.getElementById("error-message");
+    if (errorBox) {
+        errorBox.style.opacity = "0";
+        setTimeout(() => errorBox.remove(), 500);
+    }
+}, 3000);
