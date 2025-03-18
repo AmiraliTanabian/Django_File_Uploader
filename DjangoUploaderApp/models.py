30124046ab6b0f21b1d140923f8239e635476281
Amirali <atanabain@gmail.com>
2025-03-19 02:55:02 +0330

    Create __str__ for model

--- DjangoUploaderApp/models.py
+++ DjangoUploaderApp/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 class FileUploaderModel(models.Model):
     file = models.FileField(upload_to="Files/", null=True)
-    
+
+    def __str__(self):
+        return self.file.name
