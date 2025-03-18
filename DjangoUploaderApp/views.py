a8ab79fc8e35673e49cb8226685c3a3736ed0ec2
Amirali <atanabain@gmail.com>
2025-03-19 02:19:50 +0330

    Create main view

--- DjangoUploaderApp/views.py
+++ DjangoUploaderApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import FileUploadModelForm
 
-# Create your views here.
+class FileUploaderView(CreateView):
+    form_class = FileUploadModelForm
+    success_url = reverse_lazy("upload_file_name")
+    template_name = "index.html"
+    context_object_name = "form"
